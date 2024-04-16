580bbf07fb4874ce70446e7fea55a165492ecdc4
Appdruha <121567209+Appdruha@users.noreply.github.com>
2024-04-16 23:21:21 +0500

    Готово

---
+++ client/src/components/controlled-date-picker/Controlled-date-picker.tsx
@@ -0,0 +1,41 @@
+import { Controller, FieldValues, RegisterOptions } from 'react-hook-form'
+import { TextInput } from '@gravity-ui/uikit'
+
+interface ControlledInputProps {
+  name: string
+  type: 'number' | 'search' | 'text' | 'tel' | 'url' | 'email' | 'password' | undefined
+  placeholder: string
+  rules?: Omit<
+    RegisterOptions<FieldValues, string>,
+    'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'
+  >
+}
+
+export const ControlledInput = (
+  {
+    name,
+    type = 'text',
+    placeholder,
+    rules = { required: 'Поле не заполнено' },
+  }: ControlledInputProps) => {
+  return (
+    <Controller
+      name={name}
+      rules={rules}
+      render={({ field: { value = '', onChange, onBlur }, fieldState: { error } }) => (
+        <TextInput
+          size='l'
+          style={{marginTop: '16px'}}
+          errorPlacement='inside'
+          placeholder={placeholder}
+          type={type}
+          errorMessage={error?.message}
+          validationState={error && 'invalid'}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      )}
+    />
+  )
+}
---
+++ client/src/models/Request-data.ts
@@ -0,0 +1,10 @@
+export interface Request {
+  requestNumber: string
+  date: Date
+  companyName: string
+  transporterName: string
+  transporterPhone: string
+  comments: string
+  requestStatus: string
+  ATICode: string
+}
